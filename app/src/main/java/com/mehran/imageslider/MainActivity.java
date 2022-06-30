0a56909208b377c65420a31de4bf1869fcf0bcd5
mehran-abbas <mehranabbas@icloud.com>
2022-07-01 01:42:33 +0500

    new repository testing Auto Image slider using Url links

--- app/src/main/java/com/mehran/imageslider/MainActivity.java
+++ app/src/main/java/com/mehran/imageslider/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // we are creating array list for storing our image urls.
         ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
 
