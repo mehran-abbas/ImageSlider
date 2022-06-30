2531bc4f5d3a5e2aa0f7cd4f483db107dd248a70
mehran-abbas <mehranabbas@icloud.com>
2022-07-01 01:39:41 +0500

    new repository testing Auto Image slider using Url links

---
+++ app/src/main/java/com/mehran/imageslider/MainActivity.java
@@ -0,0 +1,51 @@
+package com.mehran.imageslider;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.smarteist.autoimageslider.SliderView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    // Urls of our images.
+    String url1 = "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw5bb15688/images/1170000/1178752.jpg?sfrm=jpg";
+    String url2 = "https://i.ytimg.com/vi/lqlekMT-OuA/maxresdefault.jpg";
+    String url3 = "https://royalzig.com/wp-content/uploads/2020/06/Luxury-Gold-Carving-Bed.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // we are creating array list for storing our image urls.
+        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
+
+        // initializing the slider view.
+        SliderView sliderView = findViewById(R.id.slider);
+
+        // adding the urls inside array list
+        sliderDataArrayList.add(new SliderData(url1));
+        sliderDataArrayList.add(new SliderData(url2));
+        sliderDataArrayList.add(new SliderData(url3));
+
+        // passing this array list inside our adapter class.
+        SliderAdapter adapter = new SliderAdapter(this, sliderDataArrayList);
+
+        // below method is used to set auto cycle direction in left to
+        // right direction you can change according to requirement.
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+        // below method is used to
+        // setadapter to sliderview.
+        sliderView.setSliderAdapter(adapter);
+
+        // below method is use to set
+        // scroll time in seconds.
+        sliderView.setScrollTimeInSec(3);
+
+        // to set it scrollable automatically
+        // we use below method.
+        sliderView.setAutoCycle(true);
+
+        // to start autocycle below method is used.
+        sliderView.startAutoCycle();
+    }
+}
